3abce038294bb2fbc869beb486dd35595827d8ef
Genis Bardales <genis96@yahoo.com>
2021-10-02 19:59:28 -0400

    simple style

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import Currency from './Currency';
 
 function App() {
 
-  const [currency, setCurrency] = useState([]);
-  const [conversion, setConversion] = useState([]);
-
-  useEffect(() => {
-    axios.get('https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=usd&to=cad&amount=1')
-    .then(res => {
-      setCurrency(res.data);
-      console.log(res.data);
-    }).catch(error => console.log(error));
-  }, [])
-
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <h1>Currency Converter</h1>
+      <Currency />
+      <div className='equals'>=</div>
+      <Currency />
     </div>
   );
 }
---
+++ src/Currency.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Currency() {
+    return (
+        <div>
+            <input type='number' className='input'/>
+            <select>
+                <option value='Hi'>xx</option>
+            </select>
+        </div>
+    )
+}
+
+export default Currency;
